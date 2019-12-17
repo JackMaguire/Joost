9c690b54ddf364d1e0754cbb9e4c4b52db0b530f
Jack Maguire <jackmaguire1444@gmail.com>
2019-12-16 21:00:25 -0500

    this seems to work if you use the right compiler

--- graph/test_compile.cc
+++ graph/test_compile.cc
@@ -26,15 +26,42 @@ run( N ){
   std::cout << g.num_nodes() << " nodes and " << g.num_edges() << " edges." << std::endl;
 }
 
+template< template< typename N, typename E > class G, typename N, typename E = EdgeBase >
+void
+run(){
+  constexpr int NODECOUNT = 10;
+  G< N, E > g( NODECOUNT );
+
+  std::cout << g.num_nodes() << " nodes and " << g.num_edges() << " edges." << std::endl;
+
+  for( int i = 0; i < NODECOUNT; ++i ){
+    for( int j = i + 1; j < NODECOUNT; j += i + 1 ){
+      g.add_edge( i, j );
+    }
+  }
+
+  std::cout << g.num_nodes() << " nodes and " << g.num_edges() << " edges." << std::endl;
+}
+
+//this works with gcc version 7.4.0
+//gcc test_compile.cc -std=c++1z -o test -lstdc++
+
 int main(){
 
-  FlatNodeBase< EdgeBase, std::vector > n( node_id_int( 0 ) );
-  run< MapGraph >( n );
+  {
+    FlatNodeBase< EdgeBase, std::vector > n( node_id_int( 0 ) );
+    run< MapGraph >( n );
+
+    //run< MapGraph, FlatNodeBase< EdgeBase, std::vector > >( n );
+  }
+
   //run< MapGraph, FlatNodeBase >();
-  //run< MapGraph, FlatNodeBase< EdgeBase > >();
-  //run< MapGraph, FlatNodeBase< EdgeBase, std::vector > >();
-  //run< Graph, FlatNodeBase< std::list > >();
-  //run< Graph, MapNodeBase< std::map > >();
-  //run< Graph, MapNodeBase< std::unordered_map > >();
+  run< MapGraph, FlatNodeBase< EdgeBase > >();
+  run< MapGraph, FlatNodeBase< EdgeBase, std::vector > >();
+
+  run< MapGraph, FlatNodeBase< EdgeBase, std::list > >();
+
+  run< MapGraph, MapNodeBase< EdgeBase, std::map > >();
+  run< MapGraph, MapNodeBase< EdgeBase, std::unordered_map > >();
 
 }

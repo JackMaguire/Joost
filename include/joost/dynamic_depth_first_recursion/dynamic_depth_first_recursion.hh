cbbf90ddf1793ab62def280f12325b4641bdf8a7
Jack Maguire <jackmaguire1444@gmail.com>
2021-04-14 19:58:38 -0400

    more templates

--- include/joost/dynamic_depth_first_recursion/dynamic_depth_first_recursion.hh
+++ include/joost/dynamic_depth_first_recursion/dynamic_depth_first_recursion.hh
@@ -9,8 +9,8 @@ namespace joost {
 
 class DDFRNode;
 
-template< unsigned int N_POSSIBLE_MOVES >
-using DDFRNodePtr = std::unique_ptr< DDFRNode< N_POSSIBLE_MOVES > >;
+template< class StateType, class OutcomeType, unsigned int N_POSSIBLE_MOVES >
+using DDFRNodePtr = std::unique_ptr< DDFRNode< class StateType, class OutcomeType, N_POSSIBLE_MOVES > >;
 
 enum class NodeType {
   UNINITIALIZED = 0,
@@ -19,17 +19,33 @@ enum class NodeType {
   LEAF_UNFINISHED //this is only a leaf because we previously decided not to run more
 };
 
-template< unsigned int N_POSSIBLE_MOVES >
-struct DDFRNode {
-  NodeType node_type = NodeType::UNINITIALIZED;
-  std::array< DDFRNodePtr, N_POSSIBLE_MOVES > data;
+template< class StateType, class OutcomeType, unsigned int N_POSSIBLE_MOVES >
+class DDFRNode {
+private:
+  //DATA
 
-  unsigned int depth_calculated;
+  std::array< DDFRNodePtr, N_POSSIBLE_MOVES > data_;
+  StateType outgoing_state_;
+  OutcomeType outcome_;
+
+  //   Node State
+  NodeType node_type_ = NodeType::UNINITIALIZED;
+  unsigned int depth_calculated_;
+
+
+public:
+  //METHODS
+  template< class Forecaster >
+  void
+  run_forecast( StateType const incoming_state, unsigned int const move ){
+    Forecaster::forecast( incoming_state, move, outgoing_state_, outcome_ );
+  }
 };
 
-template< unsigned int N_POSSIBLE_MOVES >
+template< class StateType, class OutcomeType, unsigned int N_POSSIBLE_MOVES >
 class DDFRCache {
   std::array< DDFRNodePtr, N_POSSIBLE_MOVES > data_;
+  StateType current_state_;
 
 public:
 
@@ -40,8 +56,23 @@ public:
     data_ = std::move( data_[ move ].data );
   }
 
-  void reset(){
+  void
+  reset( StateType const & state ){
     data_.fill( nullptr );
+    current_state_ = state;
+  }
+
+  template< class Forecaster >
+  void
+  sample_to_depth( unsigned int const depth ){
+    for( unsigned int i = 0; i < data_.size(); ++i ){
+      if( data_[ i ] == nullptr ){
+	data_[ i ] = std::make_unique< DDFRNode >();
+	data_[ i ]->run_forecast< Forecaster >( i );
+      }
+
+      data_[ i ]->
+    }
   }
 
 };

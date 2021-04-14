000265fd3a5ecee0159454ce270086de98a971d7
Jack Maguire <jackmaguire1444@gmail.com>
2021-04-14 19:45:15 -0400

    reset and register

--- include/joost/dynamic_depth_first_recursion/dynamic_depth_first_recursion.hh
+++ include/joost/dynamic_depth_first_recursion/dynamic_depth_first_recursion.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <cassert>
 
 namespace joost {
 
@@ -19,11 +20,11 @@ enum class NodeType {
 };
 
 template< unsigned int N_POSSIBLE_MOVES >
-class DDFRNode {
-  NodeType node_type_ = NodeType::UNINITIALIZED;
-  std::array< DDFRNodePtr, N_POSSIBLE_MOVES > data_;
+struct DDFRNode {
+  NodeType node_type = NodeType::UNINITIALIZED;
+  std::array< DDFRNodePtr, N_POSSIBLE_MOVES > data;
 
-
+  unsigned int depth_calculated;
 };
 
 template< unsigned int N_POSSIBLE_MOVES >
@@ -31,7 +32,18 @@ class DDFRCache {
   std::array< DDFRNodePtr, N_POSSIBLE_MOVES > data_;
 
 public:
-  
+
+  void
+  register_move( unsigned int const move ){
+    assert( data_[ move ] != nullptr );
+    assert( data_[ move ].node_type != NodeType::UNINITIALIZED );
+    data_ = std::move( data_[ move ].data );
+  }
+
+  void reset(){
+    data_.fill( nullptr );
+  }
+
 };
 
 } //namespace joost
